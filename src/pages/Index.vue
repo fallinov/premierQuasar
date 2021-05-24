55be43eb7c8d2c16d1f58dd8782a738d33c5e48f
fallinov <steve.fallet@divtec.ch>
2021-05-24 18:56:14 +0200

    Démo affichage dynamique d'images

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,25 @@
 <template>
   <q-page class="padding">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-    <router-link :to="{ path: 'hello' }">Hello page</router-link>
+    <h6>Logo sauvegardé dans assets</h6>
+    <img alt="Quasar logo" :src="logoDansAssets">
+
+    <h6>Logo sauvegardé dans statics (recommandé pour les images)</h6>
+    <img alt="Quasar logo" :src="logoDansStatics">
+
+    <div>
+      <router-link :to="{ path: 'hello' }">Hello page</router-link>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      logoDansAssets: require('assets/quasar-logo-full.svg'),
+      logoDansStatics: 'statics/quasar-logo-full.svg'
+    }
+  },
 }
 </script>
